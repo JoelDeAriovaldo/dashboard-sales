53b9363cc09081614e5f0c9004c2d0c9e8212aef
JoelDeAriovaldo <joeldeariovaldodevef@gmail.com>
2024-12-03 21:32:35 +0200

    atualiza estrutura do projeto com novas rotas e estilização aprimorada

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
 
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="min-h-screen">
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }

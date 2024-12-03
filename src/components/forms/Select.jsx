dd296b092ccb763f09a430baf25adb27a93edfd6
JoelDeAriovaldo <joeldeariovaldodevef@gmail.com>
2024-12-03 21:33:40 +0200

    adiciona novos componentes de formulário: DatePicker, Select e TextInput

---
+++ src/components/forms/Select.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Select = ({ label, value, onChange, options, className, ...props }) => {
+  return (
+    <div className={`mb-4 ${className}`}>
+      {label && (
+        <label className="block text-gray-700 text-sm font-bold mb-2">
+          {label}
+        </label>
+      )}
+      <select
+        value={value}
+        onChange={onChange}
+        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        {...props}
+      >
+        {options.map((option, index) => (
+          <option key={index} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default Select;
---
+++ src/components/forms/TextInput.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const TextInput = ({
+  label,
+  value,
+  onChange,
+  placeholder,
+  className,
+  ...props
+}) => {
+  return (
+    <div className={`mb-4 ${className}`}>
+      {label && (
+        <label className="block text-gray-700 text-sm font-bold mb-2">
+          {label}
+        </label>
+      )}
+      <input
+        type="text"
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        {...props}
+      />
+    </div>
+  );
+};
+
+export default TextInput;
